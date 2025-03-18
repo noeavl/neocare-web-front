261af73347f4a5fcce15806f407eb94d4ab1b267
noahx0001 <lopeznoe191119@gmail.com>
2025-03-17 19:45:14 -0600

    Not Found Listo

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../shared/logo/logo.component';
 
 @Component({
   selector: 'app-not-found',
   imports: [
-    RouterLink
+    RouterLink,
+    LogoComponent
   ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
