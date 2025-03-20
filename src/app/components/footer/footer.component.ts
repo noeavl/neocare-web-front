f45348f75a919286b1906fc6c47a03c37734553a
JuanGmz <juanalfredohd@gmail.com>
2025-03-20 00:11:49 -0600

    Landing page terminado?

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  email: string = 'neocare@gmail.com'
 }
